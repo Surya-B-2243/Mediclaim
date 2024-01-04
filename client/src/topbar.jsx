5518a92e2e684c39fc3a9ed7749f780af8c10143
Archi Halder <archihalder23026558@gmail.com>
2024-01-04 11:26:42 +0530

    made the navbar even from both sides

--- client/src/topbar.jsx
+++ client/src/topbar.jsx
@@ -1,16 +1,46 @@
-import { Navbar, Nav, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import '../public/topbar.css'
+import { Navbar, Nav, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../public/topbar.css";
 
 const AppNavbar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand className='nav-brand mr-auto ml-2'>Mediclaim</Navbar.Brand>
-      <Nav className="ml-auto mr-5 justify-content-end" style={{marginLeft:'auto',marginRight:'5px'}}>
-      <Link className='mr-2'to = "/register"><Button variant="outline-light" className="mr-2" style={{marginLeft:'5px'}}>Register</Button></Link>
-      <Link to = "/login"><Button variant="outline-light" className="mr-2" style={{marginLeft:'5px'}}>Login</Button></Link>
-      <Link to = "/"><Button variant="outline-light" style={{marginLeft:'5px'}}>Home</Button></Link>
-      <Link to = "/Prompt"><button className='search'>AI Search</button></Link>
+    <Navbar
+      bg="dark"
+      variant="dark"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "1rem",
+      }}
+    >
+      <Navbar.Brand className="nav-brand">Mediclaim</Navbar.Brand>
+      <Nav>
+        <Link className="mr-2" to="/register">
+          <Button
+            variant="outline-light"
+            className="mr-2"
+            style={{ marginLeft: "5px" }}
+          >
+            Register
+          </Button>
+        </Link>
+        <Link to="/login">
+          <Button
+            variant="outline-light"
+            className="mr-2"
+            style={{ marginLeft: "5px" }}
+          >
+            Login
+          </Button>
+        </Link>
+        <Link to="/">
+          <Button variant="outline-light" style={{ marginLeft: "5px" }}>
+            Home
+          </Button>
+        </Link>
+        <Link to="/Prompt">
+          <button className="search">AI Search</button>
+        </Link>
       </Nav>
     </Navbar>
   );
