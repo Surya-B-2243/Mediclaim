82df93d0229aaaa2a79e43ee6faf3b6c80bb6820
sai sameer d <saisameer.d@gmail.com>
2024-01-05 12:44:15 +0530

    did some ui changes

--- client/src/ClientID.jsx
+++ client/src/ClientID.jsx
@@ -11,8 +11,8 @@ function ClientID () {
 
     return (
         <div>
-             <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
-             <div className="bg-white p-3 rounded w-25">
+             <div className="d-flex justify-content-center align-items-center bg-dark vh-100">
+             <div className="bg-light p-3 rounded w-25">
 
                 <h2 className="text-center">Claim ID</h2>
                 <form>           
