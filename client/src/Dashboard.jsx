edf26f17dc8c1ec80887afc17ee30432ebf5cf45
Surya-B-2243 <89986380+Surya-B-2243@users.noreply.github.com>
2024-01-05 13:22:24 +0530

    dashboard done

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,6 +1,14 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import AppNavbar from "./topbar";
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button"
 
 
 function Dashboard() {
@@ -27,42 +35,60 @@ function Dashboard() {
     return(
         <div>    
             {(data && patientdata &&
-            <><h1>This data is from fhir test server</h1><ul>
-                <h2>Claim Details :</h2>
-                    <li>
-                        Id : {data.id}
-                    </li>
-                    <li>
-                        Billable Period: {data.billablePeriod.start} to {data.billablePeriod.end}
-                    </li>
-                    <li>
-                        Provider  : {data.provider.display}
-                    </li>
-                    <li>
-                        Insurance Company : {data.insurance[0].coverage.display}
-                    </li>
-                    <li>
-                        Total: {data.total.value} {data.total.currency}
-                    </li>
-                    <h2>Patient Details :</h2>
-                    <li>
-                        Name : {patientdata.name[0].family}
-                    </li>
-                    <li>
-                        Gender : {patientdata.gender}
-                    </li>
-                    <li>
-                        Birth Date : {patientdata.birthDate}
-                    </li>
-                    <li>
-                        Phone Number : {patientdata.telecom[0].value}
-                    </li>
-                    <li>
-                        Type : {patientdata.telecom[0].use}
-                    </li>
-                     
+            <>
+            <AppNavbar/>
+            <section id='blog' className='block blog-block'>
+              <Container fluid>
+                <h2 style={{color:"#31572c"}}>Details :</h2>
+                <Row>
+                  <Col sm={6}>
+                    <Card >
+                    <Card.Img variant="top" src="../public/lawyer-broker-hr-manager-signing-600nw-2186930821.png" />
+                    <Card.Body>
+                      <Card.Title>Claim Details</Card.Title>
+                      <Card.Text>
+                        Details Regarding the claim as per fhir norms:
+                      </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                      <ListGroup.Item>Id : {data.id}</ListGroup.Item>
+                      <ListGroup.Item>Billable Start : {data.billablePeriod.start}</ListGroup.Item>
+                      <ListGroup.Item>Billable End : {data.billablePeriod.end}</ListGroup.Item>
+                      <ListGroup.Item>Provider  : {data.provider.display}</ListGroup.Item>
+                      <ListGroup.Item>Insurance Company : {data.insurance[0].coverage.display}</ListGroup.Item>
+                      <ListGroup.Item>Total: {data.total.value} {data.total.currency}</ListGroup.Item>
+                    </ListGroup>
+                  </Card>
+                  </Col>
 
-                </ul></>
+                  <Col sm={6}>
+                    <Card >
+                    <Card.Img variant="top" src="../public/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.png" sizes=''/>
+                    <Card.Body>
+                      <Card.Title>Patient Details</Card.Title>
+                      <Card.Text>
+                      Patient Details as per FHIR norms.
+                      </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                      <ListGroup.Item>Name : {patientdata.name[0].family}</ListGroup.Item>
+                      <ListGroup.Item>Gender : {patientdata.gender}</ListGroup.Item>
+                      <ListGroup.Item>Birth Date : {patientdata.birthDate}</ListGroup.Item>
+                      <ListGroup.Item>Phone Number : {patientdata.telecom[0].value}</ListGroup.Item>
+                      <ListGroup.Item>Type : {patientdata.telecom[0].use}</ListGroup.Item>
+                    </ListGroup>
+                    <Card.Body>
+                      <Button variant="success">
+                        Claim
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                  </Col>
+
+                </Row>
+              </Container>
+            </section>
+            </>
 )}
         </div>
     )
